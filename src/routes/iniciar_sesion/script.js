8d0a8756aee5ca3c81e2f085ab0f531c8a275d39
Deportes <emmanuel.restrepobe@lola.edu.co>
2024-10-17 22:51:28 -0500

    vesi

--- src/routes/iniciar_sesion/script.js
+++ src/routes/iniciar_sesion/script.js
@@ -6,7 +6,7 @@ function validarCredenciales(event) {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
 
-    if (username === "lisiria" && password === "0293") {
+    if (username === "william" && password === "0293") {
         window.location.href = "/al_novedades;
     } else {
         alert("Usuario o contraseña incorrectos. Intente nuevamente.");
